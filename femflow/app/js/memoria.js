fb772678f33b9ce0dd36a7ee599cba66cf44e284
ricardojr <carolinebarros1010@gmail.com>
2025-10-23 19:00:34 -0300

    Update memoria.js

--- femflow/app/js/memoria.js
+++ femflow/app/js/memoria.js
@@ -96,7 +96,7 @@ function registrarTreino(id, pse) {
   salvarMemoria(id, memoria);
 
   // Envia PSE para Google Sheets via Apps Script
-  fetch('https://script.google.com/macros/s/AKfycbyrSiF2ev6gNmmSB2BtsWgFNV24-0nZWjl5MqBd7RwceMfs0ClQGOqG_AQ1McfvWcQ/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbyJA_kLQrDT-vLgqRhdGcgJ4IbsouXE8yM5jjDzxksWBuOmw7JbUZ47ExyiDhFjdKo9tg/exec', {
     method: 'POST',
     mode: 'no-cors',
     body: JSON.stringify({
