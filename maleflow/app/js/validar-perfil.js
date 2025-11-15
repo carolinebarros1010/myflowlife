5801454adcb3e22b4b1e0215b74adef469fb3568
ricardojr <carolinebarros1010@gmail.com>
2025-11-15 11:25:24 -0300

    Update validar-perfil.js

--- maleflow/app/js/validar-perfil.js
+++ maleflow/app/js/validar-perfil.js
@@ -3,14 +3,17 @@ import { db } from './firebase.js';
 import { doc, getDoc } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js';
 import { detectarUsuario } from './firebase.js';
 
-// Se não houver nome e registro, redireciona para cadastro.html
+const currentPage = window.location.pathname.replace(/^\//, '');
+const nextURL = encodeURIComponent(currentPage);
+
+// Se não houver nome e registro, redireciona para cadastro.html com ?next=paginaAtual
 detectarUsuario(async user => {
   if (user) {
     const ref = doc(db, `usuarios/${user.uid}`);
     const snap = await getDoc(ref);
     if (!snap.exists() || !snap.data().nome || !snap.data().registro) {
       alert("👋 Você precisa completar seu perfil antes de treinar.");
-      window.location.href = "cadastro.html";
+      window.location.href = `cadastro.html?next=${nextURL}`;
     }
   } else {
     alert("Você precisa estar logado.");
