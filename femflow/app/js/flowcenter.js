ee62cb5593c26d873414bb8b5cc75c33a6814d7a
ricardojr <carolinebarros1010@gmail.com>
2026-01-15 17:46:25 -0300

    Force extra treino session to use local focus

--- femflow/app/js/flowcenter.js
+++ femflow/app/js/flowcenter.js
@@ -263,6 +263,11 @@ function initFlowCenter() {
     btn.addEventListener("click", () => {
       const enfase = btn.dataset.extraEnfase;
       if (!enfase) return;
+      const enfaseAtual = localStorage.getItem("femflow_enfase");
+      if (enfaseAtual && !FEMFLOW.engineTreino?.isExtraEnfase?.(enfaseAtual)) {
+        localStorage.setItem("femflow_enfase_base", enfaseAtual);
+      }
+      localStorage.setItem("femflow_treino_extra", "true");
       localStorage.setItem("femflow_enfase", enfase);
       fecharModalExtra();
       FEMFLOW.router("treino.html");
